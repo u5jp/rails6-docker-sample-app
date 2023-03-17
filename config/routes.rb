82b9477b34756e7c652b31f180b8b51e231e1a75
u5jp <u15jpn@gmail.com>
2023-03-17 16:03:55 +0900

    feat: Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # root 'application#hello'
   get 'static_pages/home'
   # => StaticPages#home
-
   get 'static_pages/help'
   # => StaticPages#help
-
-  root 'application#hello'
+  get 'static_pages/about'
 end
 
