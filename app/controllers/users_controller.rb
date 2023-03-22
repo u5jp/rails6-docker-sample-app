372de9e50e00c5854e986635a1def62229484710
u5jp <u15jpn@gmail.com>
2023-03-22 21:11:18 +0900

    feat: Finish ch7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  #Get /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+
+  #Get /users/new
   def new
+    @user = User.new
+  end
+
+  def create
+    # (@user + given params).save
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # Success(valid params))
+      # GET "/users/#{user.id}"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1)
+      # => /users/1
+    else
+      # Failure(not valid params)
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
