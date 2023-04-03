6d3d0cb32817627ba87bcea5d470c94a34a64641
u5jp <u15jpn@gmail.com>
2023-04-03 18:06:08 +0900

    feat: Finish ch13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   #Get /users/:id
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   #Get /users/new
@@ -70,15 +71,6 @@ class UsersController < ApplicationController
   end
 
   # beforeアクション
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザーかどうか確認
   def correct_user
